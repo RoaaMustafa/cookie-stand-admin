fd5db7c034747bddb9e9bc6eedc432fd46f8505e
RoaaMustafa <labushanab14@gmail.com>
2021-09-13 17:18:30 +0300

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+
+    return (
+        <footer className="flex items-center justify-between p-4 bg-green-500 text-gray-50">
+            <h1 className="text-xl">&copy; 2021</h1>
+        </footer>
+    )
+}
