12f634a31a4e2ccd257ed77265f2697f80c396d5
RoaaMustafa <labushanab14@gmail.com>
2021-09-15 19:51:17 +0300

    data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,36 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Header from '../components/Header'
-import React, { useState } from 'react'
+import Footer from '../components/Footer'
 import Main from '../components/Main'
 import Link from 'next/link'
-
+import LoginForm from '../components/LoginForm'
+import { useAuth } from '../contexts/auth'
 
 export default function Home() {
-  return (
-    <div>
+  const { user, login, logout } = useAuth();
 
+  return (
+    <div className="">
       <Head>
         <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-
+      
+      
       <Header title={'Cookie Stand Admin'}>
-        <Link href="Overview" >
+        <Link href="Overviews" >
           <a>Overview</a>
         </Link>
       </Header>
+      {user ? 
+        <Main />
+        :
+        <LoginForm  login={login}/>
+      }
 
-      <Main />
-
-
-
-
+     
+      
     </div>
   )
 }
